5f93cea3c2148c87d78b27c35bd53683397cd7f8
Marcio <taboca@gmail.com>
2016-10-11 18:01:45 -0300

    f

--- client.js
+++ client.js
@@ -8,13 +8,15 @@ var client = require('electron-connect').client;
 let mainWindow;
 
 app.on('ready', () => {
+
   mainWindow = new BrowserWindow({
       height: 600,
       width: 800
   });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
- 
+
+   
   client.create(mainWindow);
 
 });
