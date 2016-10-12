717e8e017384dbdb62769365fa888e86314369ed
Marcio <taboca@gmail.com>
2016-10-12 09:59:25 -0300

    Handling unknown popups

--- client.js
+++ client.js
@@ -5,6 +5,14 @@ const {app, BrowserWindow} = require('electron');
 
 var client = require('electron-connect').client;
 
+/// 
+// This prevents possible early popups or other unknown popups
+// https://github.com/Quramy/electron-connect/issues/62
+// https://github.com/electron/electron/issues/7530
+process.on('uncaughtException', (err) => {
+  console.log('Exception:' + err);
+})
+
 let mainWindow;
 
 app.on('ready', () => {
