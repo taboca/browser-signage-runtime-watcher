9025c2d7b64c725445eb63276bd6e23fdfab2e11
Marcio <taboca@gmail.com>
2016-10-17 10:44:00 -0200

    App sample

--- 00_build/10_build.sh
+++ 00_build/10_build.sh
@@ -1,1 +1,1 @@
-electron-packager /home/telasocial/l8e/20_browser_engine/example telasocial --platform=linux --arch=x64 --version=1.4.3 --overwrite 
+electron-packager ./app telasocial --platform=linux --arch=x64 --version=1.4.3 --overwrite 
