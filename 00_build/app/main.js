cca8d23111f734d328fc2cad0a6e9f21b84ec46e
Marcio <taboca@gmail.com>
2016-10-17 10:46:16 -0200

    Generic browser

--- 00_build/app/main.js
+++ 00_build/app/main.js
@@ -1,12 +1,31 @@
 const {app, BrowserWindow} = require('electron');
 
+var config = require('./config_browser.js')
+var client = require('electron-connect').client;
+
+///
+// This prevents possible early popups or other unknown popups
+// https://github.com/Quramy/electron-connect/issues/62
+// https://github.com/electron/electron/issues/7530
+
+process.on('uncaughtException', (err, data) => {
+  console.log('Exception:' + err + ':' + data);
+})
+
 let mainWindow;
 
 app.on('ready', () => {
+
   mainWindow = new BrowserWindow({
-      height: 768,
-      width: 1360 
+      height: config.height,
+      width: config.width,
+      frame: config.frame
   });
 
-  mainWindow.loadURL('http://localhost:8888/static/10_wrappers/wide.html');
+  //mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL(config.page);
+
+  client.create(mainWindow);
+
 });
+
